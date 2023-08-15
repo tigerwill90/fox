69c8018ed67055c6f41efdb57ff67ca9e57ace25
tigerwill90 <sylvainmuller@protonmail.com>
2023-08-15 22:43:53 +0200

    feat(writer): fix test

--- context_test.go
+++ context_test.go
@@ -212,6 +212,97 @@ func TestContext_Tree(t *testing.T) {
 	f.ServeHTTP(w, req)
 }
 
+func TestContext_TeeWriter(t *testing.T) {
+	t.Parallel()
+	type capabilities struct {
+		rf  bool
+		fl  bool
+		hij bool
+		psh bool
+	}
+
+	cases := []struct {
+		name       string
+		w          ResponseWriter
+		protoMajor int
+		want       capabilities
+	}{
+		{
+			name:       "implement all h1 writer",
+			w:          h1Writer{},
+			protoMajor: 1,
+			want: capabilities{
+				rf:  true,
+				fl:  true,
+				hij: true,
+			},
+		},
+		{
+			name:       "implement all h2 writer",
+			w:          h2Writer{},
+			protoMajor: 2,
+			want: capabilities{
+				fl:  true,
+				psh: true,
+			},
+		},
+		{
+			name:       "implement only flusher for h1",
+			w:          flushWriter{},
+			protoMajor: 1,
+			want: capabilities{
+				fl: true,
+			},
+		},
+		{
+			name:       "implement only flusher for h2",
+			w:          flushWriter{},
+			protoMajor: 2,
+			want: capabilities{
+				fl: true,
+			},
+		},
+		{
+			name:       "implement only pusher",
+			w:          pushWriter{},
+			protoMajor: 2,
+			want: capabilities{
+				psh: true,
+			},
+		},
+		{
+			name:       "only rw for h1",
+			w:          multiWriter{},
+			protoMajor: 1,
+		},
+		{
+			name:       "only rw for h2",
+			w:          multiWriter{},
+			protoMajor: 2,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			req := httptest.NewRequest(http.MethodGet, "/foo", nil)
+			req.ProtoMajor = tc.protoMajor
+			c := NewTestContextOnly(New(), mockResponseWriter{}, req)
+			c.SetWriter(tc.w)
+			c.TeeWriter(bytes.NewBuffer(nil))
+			_, flOk := c.Writer().(http.Flusher)
+			_, rfOk := c.Writer().(io.ReaderFrom)
+			_, hijOk := c.Writer().(http.Hijacker)
+			_, pshOk := c.Writer().(http.Pusher)
+			assert.Equal(t, tc.want.fl, flOk)
+			assert.Equal(t, tc.want.rf, rfOk)
+			assert.Equal(t, tc.want.hij, hijOk)
+			assert.Equal(t, tc.want.psh, pshOk)
+		})
+	}
+}
+
 func TestContext_TeeWriter_h1(t *testing.T) {
 	t.Parallel()
 	const length = 1 * 1024 * 1024
