4858ca2904ba3a06e85535a85b4980fe7b32aa37
Sylvain Muller <sylvainmuller@protonmail.com>
2024-01-24 14:24:32 +0100

    feat: remove WrapM adapter (#27)

--- context.go
+++ context.go
@@ -391,19 +391,3 @@ func WrapH(h http.Handler) HandlerFunc {
 		h.ServeHTTP(c.Writer(), c.Request())
 	}
 }
-
-// WrapM is an adapter for converting http.Handler middleware into a MiddlewareFunc. It does not work with middleware
-// that wrap custom http.ResponseWriter (e.g. gzip).
-// This API is EXPERIMENTAL and is likely to change in future release.
-// Deprecated: WrapM is planned to be removed in v1.0.0.
-func WrapM(m func(handler http.Handler) http.Handler) MiddlewareFunc {
-	return func(next HandlerFunc) HandlerFunc {
-		return func(c Context) {
-			adapter := m(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				c.SetRequest(r)
-				next(c)
-			}))
-			adapter.ServeHTTP(c.Writer(), c.Request())
-		}
-	}
-}
