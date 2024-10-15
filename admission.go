007d20f5e5ac200357f87c654b103a2842e2ed7f
tigerwill90 <sylvainmuller@protonmail.com>
2024-10-15 10:36:36 +0200

    feat: wip admission controller

--- admission.go
+++ admission.go
@@ -4,6 +4,7 @@ import (
 	"cmp"
 	"context"
 	"iter"
+	"log"
 	"slices"
 )
 
@@ -14,6 +15,7 @@ type hook struct {
 type ValidationTree interface {
 	Has(method, path string) bool
 	Route(method, path string) *Route
+	Iter() Iter
 }
 
 type MutationTree interface {
@@ -34,35 +36,11 @@ type ValidationRoute interface {
 type MutationRoute interface {
 	ValidationRoute
 	SetPath(path string)
-	PatchAnnotation(fn func(annotations iter.Seq[Annotation]) iter.Seq[Annotation])
+	PatchAnnotations(fn func(annotations iter.Seq[Annotation]) iter.Seq[Annotation])
 	// TODO Patch middleware
 	SetRedirectTrailingSlash(enable bool)
 	SetIgnoreTrailingSlash(enable bool)
 	SetClientIPStrategy(strategy ClientIPStrategy)
-}
-
-type mutationRoute struct {
-	*Route
-}
-
-func (r mutationRoute) SetPath(path string) {
-	r.path = path
-}
-
-func (r mutationRoute) PatchAnnotation(fn func(annotations iter.Seq[Annotation]) iter.Seq[Annotation]) {
-	r.annots = slices.Collect(fn(slices.Values(r.annots)))
-}
-
-func (r mutationRoute) SetRedirectTrailingSlash(enable bool) {
-	r.redirectTrailingSlash = enable
-}
-
-func (r mutationRoute) SetIgnoreTrailingSlash(enable bool) {
-	r.ignoreTrailingSlash = enable
-}
-
-func (r mutationRoute) SetClientIPStrategy(strategy ClientIPStrategy) {
-	r.ipStrategy = cmp.Or(strategy, ClientIPStrategy(noClientIPStrategy{}))
 }
 
 type AdmissionController interface {
@@ -78,3 +56,62 @@ const (
 	Update
 	Delete
 )
+
+type mutationRoute struct {
+	*Route
+}
+
+func (r mutationRoute) SetPath(path string) {
+	r.path = path
+}
+
+func (r mutationRoute) PatchAnnotations(fn func(annotations iter.Seq[Annotation]) iter.Seq[Annotation]) {
+	r.annots = slices.Collect(fn(slices.Values(r.annots)))
+}
+
+func (r mutationRoute) SetRedirectTrailingSlash(enable bool) {
+	r.redirectTrailingSlash = enable
+}
+
+func (r mutationRoute) SetIgnoreTrailingSlash(enable bool) {
+	r.ignoreTrailingSlash = enable
+}
+
+func (r mutationRoute) SetClientIPStrategy(strategy ClientIPStrategy) {
+	r.ipStrategy = cmp.Or(strategy, ClientIPStrategy(noClientIPStrategy{}))
+}
+
+type validationRoute struct {
+	*Route
+}
+
+func newController() *controller {
+	c := &controller{
+		c: make(chan ValidationRoute),
+	}
+
+	go func(c <-chan ValidationRoute) {
+		for rte := range c {
+			log.Println(rte.Path())
+		}
+	}(c.c)
+
+	return c
+}
+
+type controller struct {
+	c chan ValidationRoute
+}
+
+func (c *controller) Handles(operation Operation) bool {
+	return operation == Insert || operation == Update
+}
+
+func (c *controller) Admit(ctx context.Context, tree MutationTree, route MutationRoute) (err error) {
+	return nil
+}
+
+func (c *controller) Validate(ctx context.Context, tree ValidationTree, route ValidationRoute) (err error) {
+	route.Annotations()
+	return nil
+}
